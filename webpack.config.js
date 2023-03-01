c2a88c3d6b9168e1fe37cfe71bb0dee0e6568e30
Pamfie89 <122732330+Pamfie89@users.noreply.github.com>
2023-03-01 12:28:11 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+
+module.exports = {
+  entry: "./src/index.js",
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  optimization: {
+    minimizer: ["...", new CssMinimizerPlugin()],
+  },
+  devtool: process.env.NODE_ENV === "production" ? false : "source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "static", to: "static" }],
+    }),
+  ],
+};
